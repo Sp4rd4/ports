5d871c3f9a3d386d75b511ba4cdd9ea655167f89
Sp4rd4 <petro.vizhevskyi@gmail.com>
2020-07-03 20:39:00 +0300

    Domain and proto conversions

---
+++ pkg/proto/conversions.go
@@ -0,0 +1,47 @@
+package proto
+
+import "github.com/sp4rd4/ports/pkg/domain"
+
+func PortDomainToProto(p *domain.Port) *Port {
+	if p == nil {
+		return nil
+	}
+	return &Port{
+		Id:   p.ID,
+		Name: p.Name,
+		City: p.City,
+		Code: p.Code,
+		Coordinates: &Location{
+			Latitude:  p.Coordinates.Latitude,
+			Longitude: p.Coordinates.Longitude,
+		},
+		Country:  p.Country,
+		Alias:    p.Alias,
+		Regions:  p.Regions,
+		Province: p.Province,
+		Timezone: p.Timezone,
+		Unlocs:   p.Unlocs,
+	}
+}
+
+func PortProtoToDomain(p *Port) *domain.Port {
+	if p == nil {
+		return nil
+	}
+	return &domain.Port{
+		ID:   p.Id,
+		Name: p.Name,
+		City: p.City,
+		Code: p.Code,
+		Coordinates: domain.Location{
+			Latitude:  p.Coordinates.Latitude,
+			Longitude: p.Coordinates.Longitude,
+		},
+		Country:  p.Country,
+		Alias:    p.Alias,
+		Regions:  p.Regions,
+		Province: p.Province,
+		Timezone: p.Timezone,
+		Unlocs:   p.Unlocs,
+	}
+}
