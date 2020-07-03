d482df485796e3af022a30c0fc118d438c85ac93
Sp4rd4 <petro.vizhevskyi@gmail.com>
2020-07-03 20:48:21 +0300

    GRPC client as storage

---
+++ pkg/storage/grpcclient/grpc.go
@@ -0,0 +1,54 @@
+package grpcclient
+
+import (
+	"context"
+	"fmt"
+
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+	"github.com/sp4rd4/ports/pkg/domain"
+	"github.com/sp4rd4/ports/pkg/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const errorTag = "grpc-store"
+
+type storage struct {
+	client proto.PortsClient
+}
+
+type Config struct {
+	PortsDomainHost string `env:"PORTS_DOMAIN_HOST,required"`
+}
+
+func New(conf Config) (domain.PortRepository, error) {
+	conn, err := grpc.Dial(conf.PortsDomainHost, grpc.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("[%v] portsdomain connect: %w", errorTag, err)
+	}
+
+	return &storage{client: proto.NewPortsClient(conn)}, nil
+}
+
+func (s storage) Save(port *domain.Port) error {
+	_, err := s.client.Save(context.Background(), proto.PortDomainToProto(port))
+	if err != nil {
+		return fmt.Errorf("[%v] save: %w", errorTag, err)
+	}
+	return nil
+}
+
+func (s storage) Get(id string) (*domain.Port, error) {
+	port, err := s.client.Get(context.Background(), &proto.PortRequest{Id: id})
+	if err != nil {
+		st := status.Convert(err)
+		if st.Code() == codes.NotFound {
+			return nil, fmt.Errorf("[%v] save: %w", errorTag, domain.ErrNotFound)
+		}
+		return nil, fmt.Errorf("[%v] save: %w", errorTag, err)
+	}
+
+	return proto.PortProtoToDomain(port), fmt.Errorf("[%v] save: %w", errorTag, err)
+}
